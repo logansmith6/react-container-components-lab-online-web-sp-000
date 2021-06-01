a0a9dfe71d3ac5e6f68efe586ec53b7f1eece327
Logan Smith <logsmith06@gmail.com>
2021-06-01 21:34:32 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,16 @@
 // Code MovieReviews Here
+class MovieReviews extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.nytimes.com/svc/movies/v2/reviews/all.json')
+      .then(response => response.json())
+      .then(reviewData => this.setState({reviews: reviewData.reviews}))
+  }
+}
