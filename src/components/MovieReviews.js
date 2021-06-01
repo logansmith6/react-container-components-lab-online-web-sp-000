cc01d5a0b5de3d8b4699d4b8c629a9b62726ecee
Logan Smith <logsmith06@gmail.com>
2021-06-01 21:38:11 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -18,8 +18,8 @@ class MovieReviews extends Component {
     return this.state.reviews.map(review => {
       return (
         <div className="review">
-           <img src={ review.img_url }/>
-           <h3>{ review.title }</h3>
+           <img src={ review.img_url } />
+           <h3> { review.title }</h3>
         </div>
       )
     })
